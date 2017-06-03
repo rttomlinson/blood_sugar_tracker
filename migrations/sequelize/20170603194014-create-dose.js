d2cf8a02feacd393d1a7770f5a81558592f2500f
Renzo Tomlinson <rttomlinson@gmail.com>
2017-06-03 19:43:39 +0000

    Create model and seed files for doses

---
+++ migrations/sequelize/20170603194014-create-dose.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Doses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      doseOrder: {
+        type: Sequelize.INTEGER
+      },
+      recommendedAdministration: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW")
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Doses');
+  }
+};
---
+++ models/sequelize/dose.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Dose = sequelize.define('Dose', {
+    doseOrder: DataTypes.INTEGER,
+    recommendedAdministration: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Dose;
+};
